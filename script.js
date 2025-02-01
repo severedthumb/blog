f9668ecd3db7e32ab97a076c092b9fc4ffa0307c
Jeremy <severed.thumb.studio@gmail.com>
2025-02-01 15:42:14 -0600

    create basic layout with lorem ipsum

---
+++ script.js
@@ -0,0 +1,27 @@
+// DAY MODE & NIGHT MODE CODE
+const root = document.querySelector(':root');
+const rootStyle = getComputedStyle(root);
+
+let bgColor = rootStyle.getPropertyValue('--main-bg-color');
+let fgColor = rootStyle.getPropertyValue('--main-fg-color');
+
+const modeButton = document.querySelector('.modeButton');
+const mbSlider = document.querySelector('.mbSlider');
+
+modeButton.addEventListener('click', () => {
+    if (modeButton.getAttribute('nd') == 'night') {
+        root.style.setProperty('--main-bg-color', fgColor);
+        root.style.setProperty('--main-fg-color', bgColor);
+        modeButton.setAttribute('nd', 'day');
+        modeButton.style.backgroundColor = bgColor;
+        mbSlider.style.left = '25px';
+    } else if (modeButton.getAttribute('nd') == 'day') {
+        root.style.setProperty('--main-bg-color', bgColor);
+        root.style.setProperty('--main-fg-color', fgColor);
+        modeButton.setAttribute('nd', 'night');
+        mbSlider.style.left = '5px';
+        modeButton.style.backgroundColor = fgColor;
+    }
+});
+
+
